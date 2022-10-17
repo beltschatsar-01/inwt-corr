f76e08106cef7e3523750933a71101d8e741f263
beltschatsar-01 <jecksonbadibolowa@gmail.com>
2022-10-17 20:44:21 +0300

    ddddfff2552

--- app/dbase.py
+++ app/dbase.py
@@ -23,7 +23,7 @@ while  True:
         time.sleep(2)
         
 SQLALCHEMY_DATABASE_URL =f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
-
+{settings.DATABASE_URL}
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
